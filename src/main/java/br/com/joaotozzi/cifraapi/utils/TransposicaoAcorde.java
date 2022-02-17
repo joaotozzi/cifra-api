19956369d31c8353841c22d11bc4e25fec8fc0fb
João Tozzi <joaotozzi@gmail.com>
2022-02-16 23:52:53 -0300

    ajuste no ArrayList

--- src/main/java/br/com/joaotozzi/cifraapi/utils/TransposicaoAcorde.java
+++ src/main/java/br/com/joaotozzi/cifraapi/utils/TransposicaoAcorde.java
@@ -12,43 +12,356 @@ public class TransposicaoAcorde {
 
 	public static String mudarTom(String acordeOriginal, String tomOriginal, String tomAlvo) {
 
-		if (tabelaDeTransposicao.isEmpty()){
-			tabelaDeTransposicao.put("C",
-					new ArrayList<String>(List.of("C#", "_C", "Db", "D", "D#", "_D", "Eb", "E", "E#", "Fb", "F", "F#",
-							"_G", "Gb", "G", "G#", "_A", "_G", "Ab", "A", "A#", "_A", "Bb", "B", "B#", "Cb", "C")));
-			tabelaDeTransposicao.put("Db",
-					new ArrayList<String>(List.of("D", "D#", "_D", "Eb", "E", "E#", "Fb", "F", "F#", "_F", "Gb", "G",
-							"G#", "_G", "Ab", "A", "A#", "_Ab", "_A", "Bb", "B", "B#", "Cb", "C", "C#", "_C", "Db")));
-			tabelaDeTransposicao.put("D",
-					new ArrayList<String>(List.of("D#", "_D", "Eb", "E", "E#", "Fb", "F", "F#", "_G", "Gb", "G", "G#",
-							"_A", "Ab", "A", "A#", "_B", "_A", "Bb", "B", "B#", "Cb", "C", "C#", "_D", "Db", "D")));
-			tabelaDeTransposicao.put("Eb",
-					new ArrayList<String>(List.of("E", "E#", "Fb", "F", "F#", "_F", "Gb", "G", "G#", "_G", "Ab", "A",
-							"A#", "_A", "Bb", "B", "B#", "_Bb", "Cb", "C", "C#", "_C", "Db", "D", "D#", "_D", "Eb")));
-			tabelaDeTransposicao.put("E",
-					new ArrayList<String>(List.of("E#", "Fb", "F", "F#", "_G", "Gb", "G", "G#", "_A", "Ab", "A", "A#",
-							"_B", "Bb", "B", "B#", "_C#", "Cb", "C", "C#", "_D", "Db", "D", "D#", "_E", "Eb", "E")));
-			tabelaDeTransposicao.put("F",
-					new ArrayList<String>(List.of("F#", "_F", "Gb", "G", "G#", "_G", "Ab", "A", "A#", "_A", "Bb", "B",
-							"B#", "Cb", "C", "C#", "_D", "_C", "Db", "D", "D#", "_D", "Eb", "E", "E#", "Fb", "F")));
-			tabelaDeTransposicao.put("F#",
-					new ArrayList<String>(List.of("_G", "Gb", "G", "G#", "_A", "Ab", "A", "A#", "_B", "Bb", "B", "B#",
-							"Cb", "C", "C#", "_D", "_D#", "Db", "D", "D#", "_E", "Eb", "E", "E#", "Fb", "F", "F#")));
-			tabelaDeTransposicao.put("G",
-					new ArrayList<String>(List.of("G#", "_G", "Ab", "A", "A#", "_A", "Bb", "B", "B#", "Cb", "C", "C#",
-							"_D", "Db", "D", "D#", "_E", "_D", "Eb", "E", "E#", "Fb", "F", "F#", "_G", "Gb", "G")));
-			tabelaDeTransposicao.put("Ab",
-					new ArrayList<String>(List.of("A", "A#", "_A", "Bb", "B", "B#", "Cb", "C", "C#", "_C", "Db", "D",
-							"D#", "_D", "Eb", "E", "E#", "_Eb", "Fb", "F", "F#", "_F", "Gb", "G", "G#", "_G", "Ab")));
-			tabelaDeTransposicao.put("A",
-					new ArrayList<String>(List.of("A#", "_A", "Bb", "B", "B#", "Cb", "C", "C#", "_D", "Db", "D", "D#",
-							"_E", "Eb", "E", "E#", "_F#", "Fb", "F", "F#", "_G", "Gb", "G", "G#", "_A", "Ab", "A")));
-			tabelaDeTransposicao.put("Bb",
-					new ArrayList<String>(List.of("B", "B#", "Cb", "C", "C#", "_C", "Db", "D", "D#", "_D", "Eb", "E",
-							"E#", "Fb", "F", "F#", "_G", "_F", "Gb", "G", "G#", "_G", "Ab", "A", "A#", "_A", "Bb")));
-			tabelaDeTransposicao.put("B",
-					new ArrayList<String>(List.of("B#", "Cb", "C", "C#", "_D", "Db", "D", "D#", "_E", "Eb", "E", "E#",
-							"Fb", "F", "F#", "_G", "_G#", "Gb", "G", "G#", "_A", "Ab", "A", "A#", "_B", "Bb", "B")));
+		if (tabelaDeTransposicao.isEmpty()) {
+			
+			tabelaDeTransposicao.put("A", new ArrayList<String>());
+			tabelaDeTransposicao.get("A").add("A#");
+			tabelaDeTransposicao.get("A").add("_A");
+			tabelaDeTransposicao.get("A").add("Bb");
+			tabelaDeTransposicao.get("A").add("B");
+			tabelaDeTransposicao.get("A").add("B#");
+			tabelaDeTransposicao.get("A").add("Cb");
+			tabelaDeTransposicao.get("A").add("C");
+			tabelaDeTransposicao.get("A").add("C#");
+			tabelaDeTransposicao.get("A").add("_D");
+			tabelaDeTransposicao.get("A").add("Db");
+			tabelaDeTransposicao.get("A").add("D");
+			tabelaDeTransposicao.get("A").add("D#");
+			tabelaDeTransposicao.get("A").add("_E");
+			tabelaDeTransposicao.get("A").add("Eb");
+			tabelaDeTransposicao.get("A").add("E");
+			tabelaDeTransposicao.get("A").add("E#");
+			tabelaDeTransposicao.get("A").add("_F#");
+			tabelaDeTransposicao.get("A").add("Fb");
+			tabelaDeTransposicao.get("A").add("F");
+			tabelaDeTransposicao.get("A").add("F#");
+			tabelaDeTransposicao.get("A").add("_G");
+			tabelaDeTransposicao.get("A").add("Gb");
+			tabelaDeTransposicao.get("A").add("G");
+			tabelaDeTransposicao.get("A").add("G#");
+			tabelaDeTransposicao.get("A").add("_A");
+			tabelaDeTransposicao.get("A").add("Ab");
+			tabelaDeTransposicao.get("A").add("A");
+			
+			tabelaDeTransposicao.put("Bb", new ArrayList<String>());
+			tabelaDeTransposicao.get("Bb").add("B");
+			tabelaDeTransposicao.get("Bb").add("B#");
+			tabelaDeTransposicao.get("Bb").add("Cb");
+			tabelaDeTransposicao.get("Bb").add("C");
+			tabelaDeTransposicao.get("Bb").add("C#");
+			tabelaDeTransposicao.get("Bb").add("_C");
+			tabelaDeTransposicao.get("Bb").add("Db");
+			tabelaDeTransposicao.get("Bb").add("D");
+			tabelaDeTransposicao.get("Bb").add("D#");
+			tabelaDeTransposicao.get("Bb").add("_D");
+			tabelaDeTransposicao.get("Bb").add("Eb");
+			tabelaDeTransposicao.get("Bb").add("E");
+			tabelaDeTransposicao.get("Bb").add("E#");
+			tabelaDeTransposicao.get("Bb").add("Fb");
+			tabelaDeTransposicao.get("Bb").add("F");
+			tabelaDeTransposicao.get("Bb").add("F#");
+			tabelaDeTransposicao.get("Bb").add("_G");
+			tabelaDeTransposicao.get("Bb").add("_F");
+			tabelaDeTransposicao.get("Bb").add("Gb");
+			tabelaDeTransposicao.get("Bb").add("G");
+			tabelaDeTransposicao.get("Bb").add("G#");
+			tabelaDeTransposicao.get("Bb").add("_G");
+			tabelaDeTransposicao.get("Bb").add("Ab");
+			tabelaDeTransposicao.get("Bb").add("A");
+			tabelaDeTransposicao.get("Bb").add("A#");
+			tabelaDeTransposicao.get("Bb").add("_A");
+			tabelaDeTransposicao.get("Bb").add("Bb");
+			
+			tabelaDeTransposicao.put("B", new ArrayList<String>());
+			tabelaDeTransposicao.get("B").add("B#");
+			tabelaDeTransposicao.get("B").add("Cb");
+			tabelaDeTransposicao.get("B").add("C");
+			tabelaDeTransposicao.get("B").add("C#");
+			tabelaDeTransposicao.get("B").add("_D");
+			tabelaDeTransposicao.get("B").add("Db");
+			tabelaDeTransposicao.get("B").add("D");
+			tabelaDeTransposicao.get("B").add("D#");
+			tabelaDeTransposicao.get("B").add("_E");
+			tabelaDeTransposicao.get("B").add("Eb");
+			tabelaDeTransposicao.get("B").add("E");
+			tabelaDeTransposicao.get("B").add("E#");
+			tabelaDeTransposicao.get("B").add("Fb");
+			tabelaDeTransposicao.get("B").add("F");
+			tabelaDeTransposicao.get("B").add("F#");
+			tabelaDeTransposicao.get("B").add("_G");
+			tabelaDeTransposicao.get("B").add("_G#");
+			tabelaDeTransposicao.get("B").add("Gb");
+			tabelaDeTransposicao.get("B").add("G");
+			tabelaDeTransposicao.get("B").add("G#");
+			tabelaDeTransposicao.get("B").add("_A");
+			tabelaDeTransposicao.get("B").add("Ab");
+			tabelaDeTransposicao.get("B").add("A");
+			tabelaDeTransposicao.get("B").add("A#");
+			tabelaDeTransposicao.get("B").add("_B");
+			tabelaDeTransposicao.get("B").add("Bb");
+			tabelaDeTransposicao.get("B").add("B");
+			
+			tabelaDeTransposicao.put("C", new ArrayList<String>());
+			tabelaDeTransposicao.get("C").add("C#");
+			tabelaDeTransposicao.get("C").add("_C");
+			tabelaDeTransposicao.get("C").add("Db");
+			tabelaDeTransposicao.get("C").add("D");
+			tabelaDeTransposicao.get("C").add("D#");
+			tabelaDeTransposicao.get("C").add("_D");
+			tabelaDeTransposicao.get("C").add("Eb");
+			tabelaDeTransposicao.get("C").add("E");
+			tabelaDeTransposicao.get("C").add("E#");
+			tabelaDeTransposicao.get("C").add("Fb");
+			tabelaDeTransposicao.get("C").add("F");
+			tabelaDeTransposicao.get("C").add("F#");
+			tabelaDeTransposicao.get("C").add("_G");
+			tabelaDeTransposicao.get("C").add("Gb");
+			tabelaDeTransposicao.get("C").add("G");
+			tabelaDeTransposicao.get("C").add("G#");
+			tabelaDeTransposicao.get("C").add("_A");
+			tabelaDeTransposicao.get("C").add("_G");
+			tabelaDeTransposicao.get("C").add("Ab");
+			tabelaDeTransposicao.get("C").add("A");
+			tabelaDeTransposicao.get("C").add("A#");
+			tabelaDeTransposicao.get("C").add("_A");
+			tabelaDeTransposicao.get("C").add("Bb");
+			tabelaDeTransposicao.get("C").add("B");
+			tabelaDeTransposicao.get("C").add("B#");
+			tabelaDeTransposicao.get("C").add("Cb");
+			tabelaDeTransposicao.get("C").add("C");
+					
+			tabelaDeTransposicao.put("Db", new ArrayList<String>());
+			tabelaDeTransposicao.get("Db").add("D");
+			tabelaDeTransposicao.get("Db").add("D#");
+			tabelaDeTransposicao.get("Db").add("_D");
+			tabelaDeTransposicao.get("Db").add("Eb");
+			tabelaDeTransposicao.get("Db").add("E");
+			tabelaDeTransposicao.get("Db").add("E#");
+			tabelaDeTransposicao.get("Db").add("Fb");
+			tabelaDeTransposicao.get("Db").add("F");
+			tabelaDeTransposicao.get("Db").add("F#");
+			tabelaDeTransposicao.get("Db").add("_F");
+			tabelaDeTransposicao.get("Db").add("Gb");
+			tabelaDeTransposicao.get("Db").add("G");
+			tabelaDeTransposicao.get("Db").add("G#");
+			tabelaDeTransposicao.get("Db").add("_G");
+			tabelaDeTransposicao.get("Db").add("Ab");
+			tabelaDeTransposicao.get("Db").add("A");
+			tabelaDeTransposicao.get("Db").add("A#");
+			tabelaDeTransposicao.get("Db").add("_Ab");
+			tabelaDeTransposicao.get("Db").add("_A");
+			tabelaDeTransposicao.get("Db").add("Bb");
+			tabelaDeTransposicao.get("Db").add("B");
+			tabelaDeTransposicao.get("Db").add("B#");
+			tabelaDeTransposicao.get("Db").add("Cb");
+			tabelaDeTransposicao.get("Db").add("C");
+			tabelaDeTransposicao.get("Db").add("C#");
+			tabelaDeTransposicao.get("Db").add("_C");
+			tabelaDeTransposicao.get("Db").add("Db");
+			
+			tabelaDeTransposicao.put("D", new ArrayList<String>());
+			tabelaDeTransposicao.get("D").add("D#");
+			tabelaDeTransposicao.get("D").add("_D");
+			tabelaDeTransposicao.get("D").add("Eb");
+			tabelaDeTransposicao.get("D").add("E");
+			tabelaDeTransposicao.get("D").add("E#");
+			tabelaDeTransposicao.get("D").add("Fb");
+			tabelaDeTransposicao.get("D").add("F");
+			tabelaDeTransposicao.get("D").add("F#");
+			tabelaDeTransposicao.get("D").add("_G");
+			tabelaDeTransposicao.get("D").add("Gb");
+			tabelaDeTransposicao.get("D").add("G");
+			tabelaDeTransposicao.get("D").add("G#");
+			tabelaDeTransposicao.get("D").add("_A");
+			tabelaDeTransposicao.get("D").add("Ab");
+			tabelaDeTransposicao.get("D").add("A");
+			tabelaDeTransposicao.get("D").add("A#");
+			tabelaDeTransposicao.get("D").add("_B");
+			tabelaDeTransposicao.get("D").add("_A");
+			tabelaDeTransposicao.get("D").add("Bb");
+			tabelaDeTransposicao.get("D").add("B");
+			tabelaDeTransposicao.get("D").add("B#");
+			tabelaDeTransposicao.get("D").add("Cb");
+			tabelaDeTransposicao.get("D").add("C");
+			tabelaDeTransposicao.get("D").add("C#");
+			tabelaDeTransposicao.get("D").add("_D");
+			tabelaDeTransposicao.get("D").add("Db");
+			tabelaDeTransposicao.get("D").add("D");
+			
+			tabelaDeTransposicao.put("Eb", new ArrayList<String>());
+			tabelaDeTransposicao.get("Eb").add("E");
+			tabelaDeTransposicao.get("Eb").add("E#");
+			tabelaDeTransposicao.get("Eb").add("Fb");
+			tabelaDeTransposicao.get("Eb").add("F");
+			tabelaDeTransposicao.get("Eb").add("F#");
+			tabelaDeTransposicao.get("Eb").add("_F");
+			tabelaDeTransposicao.get("Eb").add("Gb");
+			tabelaDeTransposicao.get("Eb").add("G");
+			tabelaDeTransposicao.get("Eb").add("G#");
+			tabelaDeTransposicao.get("Eb").add("_G");
+			tabelaDeTransposicao.get("Eb").add("Ab");
+			tabelaDeTransposicao.get("Eb").add("A");
+			tabelaDeTransposicao.get("Eb").add("A#");
+			tabelaDeTransposicao.get("Eb").add("_A");
+			tabelaDeTransposicao.get("Eb").add("Bb");
+			tabelaDeTransposicao.get("Eb").add("B");
+			tabelaDeTransposicao.get("Eb").add("B#");
+			tabelaDeTransposicao.get("Eb").add("_Bb");
+			tabelaDeTransposicao.get("Eb").add("Cb");
+			tabelaDeTransposicao.get("Eb").add("C");
+			tabelaDeTransposicao.get("Eb").add("C#");
+			tabelaDeTransposicao.get("Eb").add("_C");
+			tabelaDeTransposicao.get("Eb").add("Db");
+			tabelaDeTransposicao.get("Eb").add("D");
+			tabelaDeTransposicao.get("Eb").add("D#");
+			tabelaDeTransposicao.get("Eb").add("_D");
+			tabelaDeTransposicao.get("Eb").add("Eb");
+			
+			tabelaDeTransposicao.put("E", new ArrayList<String>());
+			tabelaDeTransposicao.get("E").add("E#");
+			tabelaDeTransposicao.get("E").add("Fb");
+			tabelaDeTransposicao.get("E").add("F");
+			tabelaDeTransposicao.get("E").add("F#");
+			tabelaDeTransposicao.get("E").add("_G");
+			tabelaDeTransposicao.get("E").add("Gb");
+			tabelaDeTransposicao.get("E").add("G");
+			tabelaDeTransposicao.get("E").add("G#");
+			tabelaDeTransposicao.get("E").add("_A");
+			tabelaDeTransposicao.get("E").add("Ab");
+			tabelaDeTransposicao.get("E").add("A");
+			tabelaDeTransposicao.get("E").add("A#");
+			tabelaDeTransposicao.get("E").add("_B");
+			tabelaDeTransposicao.get("E").add("Bb");
+			tabelaDeTransposicao.get("E").add("B");
+			tabelaDeTransposicao.get("E").add("B#");
+			tabelaDeTransposicao.get("E").add("_C#");
+			tabelaDeTransposicao.get("E").add("Cb");
+			tabelaDeTransposicao.get("E").add("C");
+			tabelaDeTransposicao.get("E").add("C#");
+			tabelaDeTransposicao.get("E").add("_D");
+			tabelaDeTransposicao.get("E").add("Db");
+			tabelaDeTransposicao.get("E").add("D");
+			tabelaDeTransposicao.get("E").add("D#");
+			tabelaDeTransposicao.get("E").add("_E");
+			tabelaDeTransposicao.get("E").add("Eb");
+			tabelaDeTransposicao.get("E").add("E");
+			
+			tabelaDeTransposicao.put("F", new ArrayList<String>());
+			tabelaDeTransposicao.get("F").add("F#");
+			tabelaDeTransposicao.get("F").add("_F");
+			tabelaDeTransposicao.get("F").add("Gb");
+			tabelaDeTransposicao.get("F").add("G");
+			tabelaDeTransposicao.get("F").add("G#");
+			tabelaDeTransposicao.get("F").add("_G");
+			tabelaDeTransposicao.get("F").add("Ab");
+			tabelaDeTransposicao.get("F").add("A");
+			tabelaDeTransposicao.get("F").add("A#");
+			tabelaDeTransposicao.get("F").add("_A");
+			tabelaDeTransposicao.get("F").add("Bb");
+			tabelaDeTransposicao.get("F").add("B");
+			tabelaDeTransposicao.get("F").add("B#");
+			tabelaDeTransposicao.get("F").add("Cb");
+			tabelaDeTransposicao.get("F").add("C");
+			tabelaDeTransposicao.get("F").add("C#");
+			tabelaDeTransposicao.get("F").add("_D");
+			tabelaDeTransposicao.get("F").add("_C");
+			tabelaDeTransposicao.get("F").add("Db");
+			tabelaDeTransposicao.get("F").add("D");
+			tabelaDeTransposicao.get("F").add("D#");
+			tabelaDeTransposicao.get("F").add("_D");
+			tabelaDeTransposicao.get("F").add("Eb");
+			tabelaDeTransposicao.get("F").add("E");
+			tabelaDeTransposicao.get("F").add("E#");
+			tabelaDeTransposicao.get("F").add("Fb");
+			tabelaDeTransposicao.get("F").add("F");
+			
+			tabelaDeTransposicao.put("F#", new ArrayList<String>());
+			tabelaDeTransposicao.get("F#").add("_G");
+			tabelaDeTransposicao.get("F#").add("Gb");
+			tabelaDeTransposicao.get("F#").add("G");
+			tabelaDeTransposicao.get("F#").add("G#");
+			tabelaDeTransposicao.get("F#").add("_A");
+			tabelaDeTransposicao.get("F#").add("Ab");
+			tabelaDeTransposicao.get("F#").add("A");
+			tabelaDeTransposicao.get("F#").add("A#");
+			tabelaDeTransposicao.get("F#").add("_B");
+			tabelaDeTransposicao.get("F#").add("Bb");
+			tabelaDeTransposicao.get("F#").add("B");
+			tabelaDeTransposicao.get("F#").add("B#");
+			tabelaDeTransposicao.get("F#").add("Cb");
+			tabelaDeTransposicao.get("F#").add("C");
+			tabelaDeTransposicao.get("F#").add("C#");
+			tabelaDeTransposicao.get("F#").add("_D");
+			tabelaDeTransposicao.get("F#").add("_D#");
+			tabelaDeTransposicao.get("F#").add("Db");
+			tabelaDeTransposicao.get("F#").add("D");
+			tabelaDeTransposicao.get("F#").add("D#");
+			tabelaDeTransposicao.get("F#").add("_E");
+			tabelaDeTransposicao.get("F#").add("Eb");
+			tabelaDeTransposicao.get("F#").add("E");
+			tabelaDeTransposicao.get("F#").add("E#");
+			tabelaDeTransposicao.get("F#").add("Fb");
+			tabelaDeTransposicao.get("F#").add("F");
+			tabelaDeTransposicao.get("F#").add("F#");
+			
+			tabelaDeTransposicao.put("G", new ArrayList<String>());
+			tabelaDeTransposicao.get("G").add("G#");
+			tabelaDeTransposicao.get("G").add("_G");
+			tabelaDeTransposicao.get("G").add("Ab");
+			tabelaDeTransposicao.get("G").add("A");
+			tabelaDeTransposicao.get("G").add("A#");
+			tabelaDeTransposicao.get("G").add("_A");
+			tabelaDeTransposicao.get("G").add("Bb");
+			tabelaDeTransposicao.get("G").add("B");
+			tabelaDeTransposicao.get("G").add("B#");
+			tabelaDeTransposicao.get("G").add("Cb");
+			tabelaDeTransposicao.get("G").add("C");
+			tabelaDeTransposicao.get("G").add("C#");
+			tabelaDeTransposicao.get("G").add("_D");
+			tabelaDeTransposicao.get("G").add("Db");
+			tabelaDeTransposicao.get("G").add("D");
+			tabelaDeTransposicao.get("G").add("D#");
+			tabelaDeTransposicao.get("G").add("_E");
+			tabelaDeTransposicao.get("G").add("_D");
+			tabelaDeTransposicao.get("G").add("Eb");
+			tabelaDeTransposicao.get("G").add("E");
+			tabelaDeTransposicao.get("G").add("E#");
+			tabelaDeTransposicao.get("G").add("Fb");
+			tabelaDeTransposicao.get("G").add("F");
+			tabelaDeTransposicao.get("G").add("F#");
+			tabelaDeTransposicao.get("G").add("_G");
+			tabelaDeTransposicao.get("G").add("Gb");
+			tabelaDeTransposicao.get("G").add("G");
+			
+			tabelaDeTransposicao.put("Ab", new ArrayList<String>());
+			tabelaDeTransposicao.get("Ab").add("A");
+			tabelaDeTransposicao.get("Ab").add("A#");
+			tabelaDeTransposicao.get("Ab").add("_A");
+			tabelaDeTransposicao.get("Ab").add("Bb");
+			tabelaDeTransposicao.get("Ab").add("B");
+			tabelaDeTransposicao.get("Ab").add("B#");
+			tabelaDeTransposicao.get("Ab").add("Cb");
+			tabelaDeTransposicao.get("Ab").add("C");
+			tabelaDeTransposicao.get("Ab").add("C#");
+			tabelaDeTransposicao.get("Ab").add("_C");
+			tabelaDeTransposicao.get("Ab").add("Db");
+			tabelaDeTransposicao.get("Ab").add("D");
+			tabelaDeTransposicao.get("Ab").add("D#");
+			tabelaDeTransposicao.get("Ab").add("_D");
+			tabelaDeTransposicao.get("Ab").add("Eb");
+			tabelaDeTransposicao.get("Ab").add("E");
+			tabelaDeTransposicao.get("Ab").add("E#");
+			tabelaDeTransposicao.get("Ab").add("_Eb");
+			tabelaDeTransposicao.get("Ab").add("Fb");
+			tabelaDeTransposicao.get("Ab").add("F");
+			tabelaDeTransposicao.get("Ab").add("F#");
+			tabelaDeTransposicao.get("Ab").add("_F");
+			tabelaDeTransposicao.get("Ab").add("Gb");
+			tabelaDeTransposicao.get("Ab").add("G");
+			tabelaDeTransposicao.get("Ab").add("G#");
+			tabelaDeTransposicao.get("Ab").add("_G");
+			tabelaDeTransposicao.get("Ab").add("Ab");	
+
 		}
 
 		List<String> notasAcordeOriginal = extrairNotas(acordeOriginal);
